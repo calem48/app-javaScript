106ed872f224a7baa00287b232e7d5227eaf7f6d
calem48 <messaoud.messaoud92@gmail.com>
2022-06-23 21:37:11 +0100

    update app1

--- app1/js/js.js
+++ app1/js/js.js
@@ -1,71 +1,108 @@
 let btnAdd = document.querySelector("form input:last-child")
-// let btnAdd = document.getElementsByClassName("add")
 let input = document.querySelector("form input:first-child")
 let btnRemoveAll = document.querySelector(".remove")
 let list = document.querySelector("ul")
-let listLi = document.querySelectorAll("ul li")
 
-// let data = []
-//or use 
-data = [
-    {
-        id: 1,
-        course: "HTML"
-    },
-    {
-        id: 2,
-        course: "CSS"
-    },
-    {
-        id: 3,
-        course: "JS"
-    }
-]
-
-
-
-
-addEventListener("DOMContentLoaded", function () {
-    showList(data)
-    remove()
-})
-
-function showList(data) {
-    //show item
-    data.length ?
-        list.innerHTML = data.map((item) => {
-            return `<li data-id="${item.id}"><span>${item.course}</span><button>remove</button></li>`
-        }).join("")
-        : list.innerHTML = "<li>there are no item</li>"
-
-    //remove item
-    remove()
-}
 
 
 btnAdd.addEventListener("click", function (e) {
     e.preventDefault()
     if (input.value) {
-        data = [...data, { id: new Date().getTime(), course: input.value }]
-        showList(data)
-        input.value = ""
+        showList(input.value)
     } else {
-        alert("can not add empty value")
+        alert("can't add empty value")
     }
-
+    input.value = ""
+})
+list.addEventListener("click", function (e) {
+    let id = e.target.parentNode.dataset.id
+    if (id) {
+        e.target.parentNode.remove()
+    }
 })
 
-function remove() {
-    list.querySelectorAll("li").forEach(item => {
-        item.addEventListener("click", function () {
-            data = data.filter(filt => filt.id != item.dataset.id)
-            showList(data)
-        })
-    })
+
+function showList(data) {
+    list.innerHTML += `<li data-id="${data}"><span>${data}</span><button>remove</button></li>`
 }
 
 
-btnRemoveAll.addEventListener("click", _ => {
-    data = []
-    showList(data)
+btnRemoveAll.addEventListener("click", function () {
+    list.innerHTML = ""
 })
+
+
+
+///////////////          or          /////////////////////////
+
+// let btnAdd = document.querySelector("form input:last-child")
+// // let btnAdd = document.getElementsByClassName("add")
+// let input = document.querySelector("form input:first-child")
+// let btnRemoveAll = document.querySelector(".remove")
+// let list = document.querySelector("ul")
+// let listLi = document.querySelectorAll("ul li")
+
+// // let data = []
+// //or use
+// data = [
+//     {
+//         id: 1,
+//         course: "HTML"
+//     },
+//     {
+//         id: 2,
+//         course: "CSS"
+//     },
+//     {
+//         id: 3,
+//         course: "JS"
+//     }
+// ]
+
+
+
+
+// addEventListener("DOMContentLoaded", function () {
+//     showList(data)
+//     remove()
+// })
+
+// function showList(data) {
+//     //show item
+//     data.length ?
+//         list.innerHTML = data.map((item) => {
+//             return `<li data-id="${item.id}"><span>${item.course}</span><button>remove</button></li>`
+//         }).join("")
+//         : list.innerHTML = "<li>there are no item</li>"
+
+//     //remove item
+//     remove()
+// }
+
+
+// btnAdd.addEventListener("click", function (e) {
+//     e.preventDefault()
+//     if (input.value) {
+//         data = [...data, { id: new Date().getTime(), course: input.value }]
+//         showList(data)
+//         input.value = ""
+//     } else {
+//         alert("can not add empty value")
+//     }
+
+// })
+
+// function remove() {
+//     list.querySelectorAll("li").forEach(item => {
+//         item.addEventListener("click", function () {
+//             data = data.filter(filt => filt.id != item.dataset.id)
+//             showList(data)
+//         })
+//     })
+// }
+
+
+// btnRemoveAll.addEventListener("click", _ => {
+//     data = []
+//     showList(data)
+// })
